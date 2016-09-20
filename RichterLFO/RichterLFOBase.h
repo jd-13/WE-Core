38630ded312fbf075c88793c268e2caef81ef5fa
jd-13 <jackd8989@gmail.com>
2016-09-20 21:03:42 +0100

    Correct headers

--- RichterLFO/RichterLFOBase.h
+++ RichterLFO/RichterLFOBase.h
@@ -5,20 +5,20 @@
  *
  *  Created:    18/01/2015
  *
- *	This file is part of Richter.
+ *	This file is part of WECore.
  *
- *  Richter is free software: you can redistribute it and/or modify
+ *  WECore is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
  *
- *  Richter is distributed in the hope that it will be useful,
+ *  WECore is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
- *  along with the Richter.  If not, see <http://www.gnu.org/licenses/>.
+ *  along with WECore.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef Richter_RichterLFOBase_h
