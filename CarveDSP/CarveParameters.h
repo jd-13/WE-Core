ede4aa9d07d26de23caa68688ec7f932e85d6d51
jd-13 <jackd8989@gmail.com>
2016-09-24 17:19:31 +0100

    Move parameters to core library

--- CarveDSP/CarveParameters.h
+++ CarveDSP/CarveParameters.h
@@ -25,10 +25,7 @@ public:
 
 const ParameterDefinition::RangedParameter<float>   PREGAIN(0, 2, 1),
                                                     POSTGAIN(0, 2, 0.5),
-                                                    TWEAK(-1, 1, 0),
-                                                    ROUTING(0, 1, 0), // 0 = SERIAL, 1 = PARALLEL
-                                                    DRYLEVEL(0, 2, 0),
-                                                    MASTERVOL(0, 2, 1);
+                                                    TWEAK(-1, 1, 0);
 const ModeParameter MODE(1, 6, 1);
 
 #endif /* CarveParameters_h */
