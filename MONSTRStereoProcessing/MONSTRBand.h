e27d00aca07a5e508a9edc0ad3ddfaee7cd5ff6d
jd-13 <jackd8989@gmail.com>
2016-11-12 10:51:11 +0000

    Addition of MONSTRCrossover class, documentation updates

--- MONSTRStereoProcessing/MONSTRBand.h
+++ MONSTRStereoProcessing/MONSTRBand.h
@@ -29,8 +29,41 @@
 #include "DspFilters/Butterworth.h"
 #include "MONSTRParameters.h"
 
+// forward declaration to allow friend class
+class MONSTRCrossover;
+
+/**
+ * Provides stereo width control of a single frequency range.
+ *
+ * It is recommended to use this class through the MONSTRCrossover class.
+ *
+ * This class contains a highpass and a lowpass filter in series (yes
+ * this is a roundabout way of creating a bandpass but this appeared to
+ * perform better), the passband can then have its stereo width increased
+ * or reduced.
+ *
+ * Internally relies on the parameters provided in MONSTRParameters.h
+ *
+ * @see MONSTRCrossover
+ */
 class MONSTRBand {
 public:
+    
+    /**
+     * Performs setup of the filters using default settings. The arguments are
+     * used to tell the MONSTRBand object which part of the frequency spectrum
+     * it will be covering as crossover filter.
+     *
+     * NOTE: The parameters cannot both be true. This will cause undefined behaviour.
+     *
+     * @param   newIsLower  If true, this tells the MONSTRBand object it is covering
+     *                      the lowest freqency range in the crossover. This will deactivate
+     *                      its highpass (low cut) filter.
+     * @param   newIsUpper  If true, this tells the MONSTRBand object it is covering
+     *                      the highest freqency range in the crossover. This will deactivate
+     *                      its lowpass (high cut) filter.
+     *
+     */
     MONSTRBand(bool newIsLower, bool newIsUpper) :  isActive(BANDSWITCH_DEFAULT),
                                                     isLower(newIsLower),
                                                     isUpper(newIsUpper),
@@ -49,127 +82,40 @@ public:
     }
 
     virtual ~MONSTRBand() {}
-
-
-    void setLowCutoff(float val) {
-        // if this is the lowest band, then do not cut the low frequencies
-        if (!isLower && !isUpper) {
-            lowCutoffHz = CROSSOVERLOWER.BoundsCheck(val);
-            lowCut1.setup(FILTER_ORDER, sampleRate, lowCutoffHz);
-            lowCut2.setup(FILTER_ORDER, sampleRate, lowCutoffHz);
-        } else if (isUpper) {
-            lowCutoffHz = CROSSOVERUPPER.BoundsCheck(val);
-            lowCut1.setup(FILTER_ORDER, sampleRate, lowCutoffHz);
-            lowCut2.setup(FILTER_ORDER, sampleRate, lowCutoffHz);
-        }
-    }
-
-    void setHighCutoff(float val) {
-        // if this is the highest band, then do not cut the high frequencies
-        if (!isLower && !isUpper) {
-            highCutoffHz = CROSSOVERUPPER.BoundsCheck(val);
-            highCut1.setup(FILTER_ORDER, sampleRate, highCutoffHz);
-            highCut2.setup(FILTER_ORDER, sampleRate, highCutoffHz);
-        } else if (isLower) {
-            highCutoffHz = CROSSOVERLOWER.BoundsCheck(val);
-            highCut1.setup(FILTER_ORDER, sampleRate, highCutoffHz);
-            highCut2.setup(FILTER_ORDER, sampleRate, highCutoffHz);
-        }
-    }
-
-
-
+    
+    friend class MONSTRCrossover;
+    
+    /**
+     * Sets the stereo width of this band.
+     * Higher value = more width
+     *
+     * @param   val   The frequency in Hz to set the width to
+     *
+     * @see     WIDTH for valid values
+     */
     void setWidth(float val) { width = WIDTH.BoundsCheck(val); }
 
+    /**
+     * Sets whether this band will modify the stereo width of the signal it receives or not.
+     *
+     * If the band is deactivated filtering will still be performed. This is so that a 
+     * crossover containing multiple MONSTRBands will always be able easily to sum the
+     * output of the bands to unity gain, whether some of the MONSTRBands are bypassed
+     * or not.
+     *
+     * @param   val If true, stereo width processing will be applied
+     */
     void setIsActive(bool val) { isActive = val; }
 
-    void setSampleRate(double newSampleRate) {
-        // if the new sample rate is different, recalculate the filter coefficients
-        if (newSampleRate != sampleRate) {
-            sampleRate = newSampleRate;
-            setLowCutoff(lowCutoffHz);
-            setHighCutoff(highCutoffHz);
-        }
-    }
-
-    float getLowCutoff() const { return lowCutoffHz;
-    }
-
-    float getHighCutoff() const { return highCutoffHz; }
-
+    /**
+     * @see setWidth
+     */
     float getWidth() const { return width; }
 
+    /**
+     * @see setIsActive
+     */
     float getIsActive() const { return isActive; }
-
-
-    /**
-     * Lets the band know if it covers the lowest frequencies, so will
-     * apply only a high cut filter.
-     */
-    void makeBandLower() {
-        isLower = true;
-        isUpper = false;
-    }
-
-    /**
-     * Lets the band know if it covers the middle frequencies, so will
-     * apply both a low and high cut filter.
-     */
-    void makeBandMiddle() {
-        isLower = false;
-        isUpper = false;
-    }
-
-    /**
-     * Lets the band know if it covers the highest frequencies, so will
-     * apply only a low cut filter.
-     */
-    void makeBandUpper() {
-        isLower = false;
-        isUpper = true;
-    }
-
-    /**
-     * Resets filter states. Call before beginning a new buffer of
-     * samples.
-     */
-    void reset() {
-        lowCut1.reset();
-        lowCut2.reset();
-        highCut1.reset();
-        highCut2.reset();
-    }
-
-    /**
-     * Performs the effect processing on inLeftSample and inRightSample. Use for
-     * stereo in->stereo out signals.
-     *
-     * @param   inLeftSamples   Reference to a vector of left samples to be processed
-     * @param   inRightSamples  Reference to a vector of right samples to be processed
-     */
-    void process2in2out(std::vector<float>& inLeftSamples,
-                                    std::vector<float>& inRightSamples) {
-
-        if (inLeftSamples.size() == inRightSamples.size()) {
-            // Apply the filtering before processing
-            filterSamples(&inLeftSamples[0], &inRightSamples[0], inLeftSamples.size());
-
-            if (isActive) {
-                // Do the actual stereo widening or narrowing
-                // Based on: http://musicdsp.org/showArchiveComment.php?ArchiveID=256
-                double coef_S {width * 0.5};
-
-                for (size_t iii {0}; iii < inLeftSamples.size(); iii++) {
-
-                    double mid {(inLeftSamples[iii] + inRightSamples[iii]) * 0.5};
-                    double side {(inRightSamples[iii] - inLeftSamples[iii]) * coef_S};
-
-                    inLeftSamples[iii] = mid - side;
-                    inRightSamples[iii] = mid + side;
-                }
-            }
-        }
-    }
 
 private:
     bool    isActive,
@@ -208,6 +154,114 @@ private:
         }
 
         delete[] channelsArray;
+    }
+    
+    /**
+     * Lets the band know if it covers the lowest frequencies, so will
+     * apply only a high cut filter.
+     */
+    void makeBandLower() {
+        isLower = true;
+        isUpper = false;
+    }
+    
+    /**
+     * Lets the band know if it covers the middle frequencies, so will
+     * apply both a low and high cut filter.
+     */
+    void makeBandMiddle() {
+        isLower = false;
+        isUpper = false;
+    }
+    
+    /**
+     * Lets the band know if it covers the highest frequencies, so will
+     * apply only a low cut filter.
+     */
+    void makeBandUpper() {
+        isLower = false;
+        isUpper = true;
+    }
+    
+    /**
+     * Resets filter states. Call before beginning a new buffer of
+     * samples.
+     */
+    void reset() {
+        lowCut1.reset();
+        lowCut2.reset();
+        highCut1.reset();
+        highCut2.reset();
+    }
+    
+    void setSampleRate(double newSampleRate) {
+        // if the new sample rate is different, recalculate the filter coefficients
+        if (newSampleRate != sampleRate) {
+            sampleRate = newSampleRate;
+            setLowCutoff(lowCutoffHz);
+            setHighCutoff(highCutoffHz);
+        }
+    }
+    
+    void setLowCutoff(float val) {
+        // if this is the lowest band, then do not cut the low frequencies
+        if (!isLower && !isUpper) {
+            lowCutoffHz = CROSSOVERLOWER.BoundsCheck(val);
+            lowCut1.setup(FILTER_ORDER, sampleRate, lowCutoffHz);
+            lowCut2.setup(FILTER_ORDER, sampleRate, lowCutoffHz);
+        } else if (isUpper) {
+            lowCutoffHz = CROSSOVERUPPER.BoundsCheck(val);
+            lowCut1.setup(FILTER_ORDER, sampleRate, lowCutoffHz);
+            lowCut2.setup(FILTER_ORDER, sampleRate, lowCutoffHz);
+        }
+    }
+    
+    void setHighCutoff(float val) {
+        // if this is the highest band, then do not cut the high frequencies
+        if (!isLower && !isUpper) {
+            highCutoffHz = CROSSOVERUPPER.BoundsCheck(val);
+            highCut1.setup(FILTER_ORDER, sampleRate, highCutoffHz);
+            highCut2.setup(FILTER_ORDER, sampleRate, highCutoffHz);
+        } else if (isLower) {
+            highCutoffHz = CROSSOVERLOWER.BoundsCheck(val);
+            highCut1.setup(FILTER_ORDER, sampleRate, highCutoffHz);
+            highCut2.setup(FILTER_ORDER, sampleRate, highCutoffHz);
+        }
+    }
+    
+    float getLowCutoff() const { return lowCutoffHz; }
+    
+    float getHighCutoff() const { return highCutoffHz; }
+    
+    /**
+     * Performs the effect processing on inLeftSample and inRightSample. Use for
+     * stereo in->stereo out signals.
+     *
+     * @param   inLeftSamples   Reference to a vector of left samples to be processed
+     * @param   inRightSamples  Reference to a vector of right samples to be processed
+     */
+    void process2in2out(std::vector<float>& inLeftSamples,
+                        std::vector<float>& inRightSamples) {
+        
+        if (inLeftSamples.size() == inRightSamples.size()) {
+            // Apply the filtering before processing
+            filterSamples(&inLeftSamples[0], &inRightSamples[0], inLeftSamples.size());
+            
+            if (isActive) {
+                // Do the actual stereo widening or narrowing
+                // Based on: http://musicdsp.org/showArchiveComment.php?ArchiveID=256
+                double coef_S {width * 0.5};
+                
+                for (size_t iii {0}; iii < inLeftSamples.size(); iii++) {
+                    
+                    double mid {(inLeftSamples[iii] + inRightSamples[iii]) * 0.5};
+                    double side {(inRightSamples[iii] - inLeftSamples[iii]) * coef_S};
+                    
+                    inLeftSamples[iii] = mid - side;
+                    inRightSamples[iii] = mid + side;
+                }
+            }
+        }
     }
 };
 
---
+++ MONSTRStereoProcessing/MONSTRCrossover.h
@@ -0,0 +1,169 @@
+/*
+ *  File:       MONSTRCrossover.h
+ *
+ *  Version:    1.0.0
+ *
+ *  Created:    08/11/2016
+ *
+ *	This file is part of WECore.
+ *
+ *  WECore is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  WECore is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with WECore.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef MONSTRCrossover_h
+#define MONSTRCrossover_h
+
+#include <vector>
+#include "MONSTRBand.h"
+
+
+/**
+ * Class which provides three band stereo width control. 
+ *
+ * This class contains three MONSTRBand objects, and may later be expanded
+ * to support a variable number of bands.
+ *
+ * To use this class, use the setter and getter methods to manipulate crossover
+ * parameters which are shared by several bands (crossover frequencies), and call
+ * the setter and getter methods of each band to manipulate parameters which are
+ * particular to each band. Call the reset, setSampleRate and process methods as
+ * required.
+ *
+ * @see MONSTRBand
+ */
+class MONSTRCrossover {
+public:
+    
+    MONSTRBand  band1,
+                band2,
+                band3;
+    
+    /**
+     * Makes each band aware of its position, and therefore which of their internal filters
+     * they each need to activate.
+     */
+    MONSTRCrossover() : band1(true, false),
+                        band2(false, false),
+                        band3(false, true) {}
+    
+    virtual ~MONSTRCrossover() {}
+    
+    /**
+     * Applies the filtering to a stereo buffer of samples.
+     * Expect seg faults or other memory issues if arguements passed are incorrect.
+     *
+     * @param   inLeftSample    Pointer to the first sample of the left channel's buffer
+     * @param   inRightSample   Pointer to the first sample of the right channel's buffer
+     * @param   numSamples      Number of samples in the buffer. The left and right buffers
+     *                          must be the same size.
+     */
+    void Process2in2out(float* leftSample, float* rightSample, int numSamples) {
+        // make a copy of the buffers for each band to process in parallel
+        std::vector<float> band1LeftBuffer(numSamples);
+        std::vector<float> band1RightBuffer(numSamples);
+        std::vector<float> band2LeftBuffer(numSamples);
+        std::vector<float> band2RightBuffer(numSamples);
+        std::vector<float> band3LeftBuffer(numSamples);
+        std::vector<float> band3RightBuffer(numSamples);
+        
+        for (size_t iii {0}; iii < numSamples; iii++) {
+            band1LeftBuffer[iii] = leftSample[iii];
+            band2LeftBuffer[iii] = leftSample[iii];
+            band3LeftBuffer[iii] = leftSample[iii];
+            
+            band1RightBuffer[iii] = rightSample[iii];
+            band2RightBuffer[iii] = rightSample[iii];
+            band3RightBuffer[iii] = rightSample[iii];
+        }
+        
+        // let each band do its processing
+        band1.process2in2out(band1LeftBuffer, band1RightBuffer);
+        band2.process2in2out(band2LeftBuffer, band2RightBuffer);
+        band3.process2in2out(band3LeftBuffer, band3RightBuffer);
+        
+        // combine the output from each band, and write to output
+        for (size_t iii {0}; iii < numSamples; iii++) {
+            leftSample[iii] = band1LeftBuffer[iii] + band2LeftBuffer[iii] + band3LeftBuffer[iii];
+            rightSample[iii] = band1RightBuffer[iii] + band2RightBuffer[iii] + band3RightBuffer[iii];
+        }
+    }
+    
+    /**
+     * Sets the crossover frequency of the lower (band1) and middle (band2) bands.
+     *
+     * @param   val   The frequency in Hz to set the lower crossover point to.
+     *
+     * @see     CROSSOVERLOWER for valid values
+     */
+    void setCrossoverLower(float val) {
+        band1.setHighCutoff(val);
+        band2.setLowCutoff(val);
+    }
+    
+    /**
+     * Sets the crossover frequency of the middle (band2) and upper (band3) bands.
+     *
+     * @param   val   The frequency in Hz to set the upper crossover point to.
+     *
+     * @see     CROSSOVERUPPER for valid values
+     */
+    void setCrossoverUpper(float val) {
+        band2.setHighCutoff(val);
+        band3.setLowCutoff(val);
+    }
+    
+    /**
+     * Gets the crossover frequency of the lower (band1) and middle (band2) bands.
+     *
+     * @return  val The frequency in Hz of the lower crossover point.
+     *
+     * @see     CROSSOVERLOWER for valid values
+     */
+    float getCrossoverLower() { return band1.getHighCutoff(); }
+    
+    /**
+     * Gets the crossover frequency of the middle (band2) and upper (band3) bands.
+     *
+     * @return  val The frequency in Hz of the upper crossover point.
+     *
+     * @see     CROSSOVERUPPER for valid values
+     */
+    float getCrossoverUpper() { return band2.getHighCutoff(); }
+    
+    /**
+     * Configures the filters for the correct sample rate. Ensure this is
+     * called before attempting to process audio.
+     *
+     * @param   newSampleRate  The sample rate the filter should be configured for
+     */
+    void setSampleRate(double newSampleRate) {
+        band1.setSampleRate(newSampleRate);
+        band2.setSampleRate(newSampleRate);
+        band3.setSampleRate(newSampleRate);
+    }
+    
+    /**
+     * Resets all filters.
+     * Call this whenever the audio stream is interrupted (ie. the playhead is moved)
+     */
+    void reset() {
+        band1.reset();
+        band2.reset();
+        band3.reset();
+    }
+};
+
+
+
+#endif /* MONSTRCrossover_h */
