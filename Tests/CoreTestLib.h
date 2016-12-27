d9bab5b5c48270fb515ea3452e617b01226321fc
jd-13 <jackd8989@gmail.com>
2016-12-27 11:26:35 +0000

    Update header comments

--- Tests/CoreTestLib.h
+++ Tests/CoreTestLib.h
@@ -1,10 +1,25 @@
-//
-//  CoreTestLib.h
-//  CarveTest
-//
-//  Created by Jack Devlin on 15/11/2016.
-//
-//
+/*
+ *  File:       CoreTestLib.h
+ *
+ *  Version:    1.0.0
+ *
+ *  Created:    15/11/2016
+ *
+ *	This file is part of WECore.
+ *
+ *  WECore is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  WECore is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with WECore.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #ifndef CoreTestLib_h
 #define CoreTestLib_h
