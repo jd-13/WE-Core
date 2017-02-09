f7d12325b22f9061690fef9dd0ac63382b192191
jd-13 <jackd8989@gmail.com>
2017-02-09 22:04:28 +0000

    Additional tests

--- Tests/SongbirdFilterModuleTests.cpp
+++ Tests/SongbirdFilterModuleTests.cpp
@@ -56,3 +56,61 @@ SCENARIO("SongbirdFilterModule: Parameters can be set and retrieved correctly") 
         }
     }
 }
+
+SCENARIO("SongbirdFilterModule: Parameters enforce their bounds correctly") {
+    GIVEN("A new SongbirdFilterModule object") {
+        SongbirdFilterModule mSongbird;
+        
+        WHEN("All parameter values are too low") {
+            mSongbird.setVowel1(-5);
+            mSongbird.setVowel2(-5);
+            mSongbird.setFilterPosition(-5);
+            mSongbird.setMix(-5);
+            
+            THEN("Parameters enforce their lower bounds") {
+                REQUIRE(mSongbird.getVowel1() == 1);
+                REQUIRE(mSongbird.getVowel2() == 1);
+                REQUIRE(mSongbird.getFilterPosition() == 0);
+                REQUIRE(mSongbird.getMix() == 0);
+            }
+        }
+        
+        WHEN("All parameter values are too high") {
+            mSongbird.setVowel1(1000);
+            mSongbird.setVowel2(1000);
+            mSongbird.setFilterPosition(1000);
+            mSongbird.setMix(1000);
+            
+            THEN("Parameters enforce their upper bounds") {
+                REQUIRE(mSongbird.getVowel1() == 5);
+                REQUIRE(mSongbird.getVowel2() == 5);
+                REQUIRE(mSongbird.getFilterPosition() == 1);
+                REQUIRE(mSongbird.getMix() == 1);
+            }
+        }
+    }
+}
+
+SCENARIO("SongbirdFilterModule: Silence in = silence out") {
+    GIVEN("A SongbirdFilterModule and a buffer of silent samples") {
+        std::vector<double> leftBuffer(1024);
+        std::vector<double> rightBuffer(1024);
+        SongbirdFilterModule mSongbird;
+        
+        WHEN("The silence samples are processed") {
+            // fill the buffer
+            std::fill(leftBuffer.begin(), leftBuffer.end(), 0);
+            std::fill(rightBuffer.begin(), rightBuffer.end(), 0);
+            
+            // do processing
+            mSongbird.Process2in2out(&leftBuffer[0], &rightBuffer[0], leftBuffer.size());
+            
+            THEN("The output is silence") {
+                for (size_t iii {0}; iii < leftBuffer.size(); iii++) {
+                    REQUIRE(CoreTestLib::compareFloats(leftBuffer[iii], 0.0));
+                    REQUIRE(CoreTestLib::compareFloats(rightBuffer[iii], 0.0));
+                }
+            }
+        }
+    }
+}
