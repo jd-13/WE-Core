4968e18470c4a17882e43e562c0b67b0f485f1ed
jd-13 <jackd8989@gmail.com>
2017-12-17 15:50:58 +0000

    Don't git clone inside docker, use the shared travis directory

--- scripts/CIBuild.sh
+++ scripts/CIBuild.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-update-alternatives --install /usr/bin/gcov gcov /usr/bin/gcov-5 90
+update-alternatives --install /usr/bin/gcov gcov /usr/bin/gcov-7 90
 
-git submodule init
-git submodule update
-export WECORE_SRC=/home/WE-Core
+export WECORE_SRC=`pwd`
 
+cd $WECORE_SRC
 mkdir build && cd build
 cmake .. && make
 if [ "$CXX" = "g++-7" ]; then $WECORE_SRC/scripts/get_code_cov.sh; fi
 
 $VALGRIND_PATH/coregrind/valgrind --tool=callgrind ./WECoreTest
 if [ "$CXX" = "g++-7" ]; then bash <(curl -s https://codecov.io/bash); fi
-mv callgrind.out.* /home/travisDir/callgrind.out.$TRAVIS_BUILD_NUMBER
+mv callgrind.out.* callgrind.out.$TRAVIS_BUILD_NUMBER
