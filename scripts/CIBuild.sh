ce989d0f7d264172f2a2acbf33ea1fb84539a2e8
jd-13 <jackd8989@gmail.com>
2017-12-17 12:06:49 +0000

    Build with both gcc and clang, remove cppcheck

--- scripts/CIBuild.sh
+++ scripts/CIBuild.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-if [ "$CXX" = "g++" ]; then export CXX="g++-5" CC="gcc-5"; fi
-if [ "$CXX" = "clang++" ]; then export CXX="clang++-5.0" CC="clang-5.0"; fi
 update-alternatives --install /usr/bin/gcov gcov /usr/bin/gcov-5 90
 
 git submodule init
@@ -10,9 +8,8 @@ export WECORE_SRC=/home/WE-Core
 
 mkdir build && cd build
 cmake .. && make
-if [ "$CXX" = "g++-5" ]; then $WECORE_SRC/scripts/get_code_cov.sh; fi
+if [ "$CXX" = "g++-7" ]; then $WECORE_SRC/scripts/get_code_cov.sh; fi
 
 $VALGRIND_PATH/coregrind/valgrind --tool=callgrind ./WECoreTest
-if [ "$CXX" = "g++-5" ]; then bash <(curl -s https://codecov.io/bash); fi
-cppcheck -i$WECORE_SRC/DSPFilters -i$WECORE_SRC/valgrind --quiet --error-exitcode=1 .
+if [ "$CXX" = "g++-7" ]; then bash <(curl -s https://codecov.io/bash); fi
 mv callgrind.out.* /home/travisDir/callgrind.out.$TRAVIS_BUILD_NUMBER
