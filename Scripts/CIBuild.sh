795420fca5d3e534a909e729fc4d1ba2b4210b9b
jd-13 <jackd8989@gmail.com>
2020-07-17 23:32:21 +0100

    CI only install gcov when using GCC

--- Scripts/CIBuild.sh
+++ Scripts/CIBuild.sh
@@ -4,7 +4,9 @@ set -e
 
 BUILD_NUMBER=$1
 
-update-alternatives --install /usr/bin/gcov gcov /usr/bin/gcov-7 90
+if [ "$CXX" = "g++-7" ]; then
+    update-alternatives --install /usr/bin/gcov gcov /usr/bin/gcov-7 90
+fi
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null 2>&1 && pwd )"
 export WECORE_HOME="$(cd $SCRIPT_DIR/.. > /dev/null 2>&1 && pwd)"
