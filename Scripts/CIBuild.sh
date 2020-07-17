9b69cddb7c51b9d96dbabcb2b6eda2fc34af2280
jd-13 <jackd8989@gmail.com>
2020-07-17 22:54:52 +0100

    Update CI Dockerfiles to ubuntu focal

--- Scripts/CIBuild.sh
+++ Scripts/CIBuild.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+BUILD_NUMBER=$1
+
 update-alternatives --install /usr/bin/gcov gcov /usr/bin/gcov-7 90
 
-export WECORE_HOME=`pwd`
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null 2>&1 && pwd )"
+export WECORE_HOME="$(cd $SCRIPT_DIR/.. > /dev/null 2>&1 && pwd)"
 export WECORE_SRC="$WECORE_HOME/WECore"
 
 cd $WECORE_HOME
