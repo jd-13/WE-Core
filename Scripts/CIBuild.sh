36f0287eb91c342c0d739b47ceb15f9cc97fcd48
jd-13 <jackd8989@gmail.com>
2020-12-07 13:13:58 +0000

    Update gcc version in script

--- Scripts/CIBuild.sh
+++ Scripts/CIBuild.sh
@@ -4,8 +4,8 @@ set -e
 
 BUILD_NUMBER=$1
 
-if [ "$CXX" = "g++-7" ]; then
-    update-alternatives --install /usr/bin/gcov gcov /usr/bin/gcov-7 90
+if [ "$CXX" = "g++-10" ]; then
+    update-alternatives --install /usr/bin/gcov gcov /usr/bin/gcov-10 90
 fi
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null 2>&1 && pwd )"
@@ -15,12 +15,12 @@ export WECORE_SRC="$WECORE_HOME/WECore"
 cd $WECORE_HOME
 mkdir build && cd build
 cmake .. && make
-if [ "$CXX" = "g++-7" ]; then
+if [ "$CXX" = "g++-10" ]; then
     $WECORE_HOME/Scripts/get_code_cov.sh;
 fi
 
 $VALGRIND_PATH/coregrind/valgrind --tool=callgrind ./WECoreTest
-if [ "$CXX" = "g++-7" ]; then
+if [ "$CXX" = "g++-10" ]; then
     bash <(curl -s https://codecov.io/bash);
 fi
 
