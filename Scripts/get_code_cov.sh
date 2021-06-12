4360c49fe87d427969d793d6a0a03af873e9b756
jd-13 <jackd8989@gmail.com>
2021-06-12 11:21:20 +0100

    Don't show gcov output in log

--- Scripts/get_code_cov.sh
+++ Scripts/get_code_cov.sh
@@ -2,5 +2,5 @@
 cd ${WECORE_SRC}
 for filename in `find . | egrep '\.cpp'`;
 do
-    gcov-10 -o ../build/CMakeFiles/WECoreTest.dir/WECore/$filename.o $filename
+    gcov-10 -o ../build/CMakeFiles/WECoreTest.dir/WECore/$filename.o $filename > /dev/null
 done
